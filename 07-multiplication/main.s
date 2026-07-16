1cd1e37b2967fbc96347c4bd008fa1c56857f593
Les Carbonaro <lcarbonaro@gmail.com>
2026-07-16 12:10:46 -0400

    add code example for lesson 7

--- 07-multiplication/main.s
+++ 07-multiplication/main.s
@@ -4,7 +4,16 @@ global _start
 _start:
 mov eax, 4   ; the first operand must be loaded in the eax register
 mov ecx, 5   ; the second operand is then loaded onto another register
-mul ecx
+mul ecx      ; eax = eax * ecx = 20
+
+; note: 
+; MUL is used for multiplying unsigned (positive) numbers
+; IMUL is used for signed numbers
+
+mov eax, -3  
+mov ecx, 5   
+imul ecx      ; eax = eax * ecx = -15
+
 
 
 int 80h

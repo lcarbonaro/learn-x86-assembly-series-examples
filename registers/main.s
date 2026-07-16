7261c1d1b772d18b58143db04dc8cecef1220575
Les Carbonaro <lcarbonaro@gmail.com>
2026-07-15 20:02:32 -0400

    add assembler examples for lessons 1 and 2

---
+++ registers/main.s
@@ -0,0 +1,22 @@
+section .text
+global _start
+
+_start:
+
+; 32-bit registers
+
+mov ebx, 12   ;put the value 12 inside the register ebx
+mov eax, 8    ;put the value 8 in eax
+mov edx, 32   ; mov <destination>,<source or value>
+
+; 16-bit registers
+; registers al, bl, cl, etc. are 
+; lower half (lower 16-bits) of eax, ebx, ecx etc.
+
+mov al, 2  ;put the value 2 inside the register al
+mov bl, 1  
+mov cl, 15
+mov dl, 10
+
+; system interrupt
+int 80h

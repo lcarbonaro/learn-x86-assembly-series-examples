37a52446fc9faaa792430081c9acf132e522fd75
Les Carbonaro <lcarbonaro@gmail.com>
2026-07-18 13:55:51 -0400

    add example code for lesson 13

--- 13-intro-to-comparisons/main.s
+++ 13-intro-to-comparisons/main.s
@@ -1,35 +1,28 @@
 section .data
 msg db   "Equal" , 0xa   ; newline at end
 msgLen   equ $-msg
-msgNE    db "Not Equal" , 0xa
-msgNELen equ $-msgNE
 
 section .text
 global _start
 
 _start:
-mov eax, 20
+mov eax, 15
 mov ebx, 15
 
 cmp eax, ebx   ; compare in effect attempts a subtract eax - ebx
-               ; operand registers not affected, only EFLAGS register
+               ; operand registers not affected in any way
+               ; EFLAGS register holds zero flag (ZF) 
+               ; if ZF bit is on, then result of CMP was zero 
+               ; i.e. equal numbers compared
 
-je  equal 
-jmp notEqual
+je  equal      ; jumps to label "equal" if CMP result was equal
+jmp exit       ; else jumps to label "exit"
 
 equal:
 mov eax, 4        ; sys_write
 mov ebx, 1        ; stdout terminal
 mov ecx, msg
 mov edx, msgLen
-int 80h
-jmp exit
-
-notEqual:
-mov eax, 4        ; sys_write
-mov ebx, 1        ; stdout terminal
-mov ecx, msgNE
-mov edx, msgNELen
 int 80h
 
 exit:
